450e68125740796e6b1311cd86107045fdaf3da6
Kroj-0 <_ganryu_@libero.it>
2015-01-25 12:34:54 +0100

    Gen Dung 2.2
    Stanze messe in maniera quasi logica, da sistemare

--- testingfisting/testingfisting/WorldGenTest.java
+++ testingfisting/testingfisting/WorldGenTest.java
@@ -4,23 +4,38 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class WorldGenTest {
-	int seed=17817;
+	int seed=111;
 	int width=50, height=50;
+	int corrWidth=2;
 	int roomsnumber=20;
 	Room rooms[];
 	ArrayList<Room> roomList = new ArrayList<Room>();
 	ArrayList<Corridor> corridorList = new ArrayList<Corridor>();
-	int max=10, min=4; //dimensioni stanze
+	int max=7, min=3; //dimensioni stanze
 	public int tempTerrain[][]= new int[width][height];
-	public int terrain[][]= new int[width*2][height*2];
+	public int terrain[][]= new int[width*3][height*3];
 	
-	public boolean inters(Room a, int[][] tempTerrain){
-		for(int i=a.x-a.leftSpace; i<a.x+a.width+a.rightSpace; i++){
-			for(int j=a.y-a.topSpace; j<a.y+a.height+a.downSpace; j++){
-				if((i>0)&&(i<width)&&(j>0)&&(j<height)) if(tempTerrain[i][j]==0) return true;
+	public boolean inters(Room a){
+		int c=0;
+		for(int i=a.x; i<a.x+a.width-1; i++){ 
+			if(tempTerrain[i][a.y-1]==0){
+				c++;
+			}
+			if(tempTerrain[i][a.y+a.height]==0){
+				c++;
 			}
 		}
-		return false;
+		for(int j=a.y; j<a.y+a.height-1; j++){ 
+			if(tempTerrain[a.x-1][j]==0){
+				c++;
+			}
+			if(tempTerrain[a.x+a.width][j]==0){
+				c++;
+			}
+		}
+		System.out.print(c+" ");
+		if((c>0)&&(c<7)) return false;
+		else return true;
 	}
 	
 	public void putRoom(Room a){
@@ -40,76 +55,62 @@ public class WorldGenTest {
 		Random r= new Random();
 		r.setSeed(seed);
 		
-		  int h, w;
+		int h, w;
 		for(int i=0; i<roomsnumber; i++){	
 			h=(int) (r.nextFloat()*(max-min)+min);
 			w=(int) (r.nextFloat()*(max-min)+min);
 			rooms[i]= new Room(h, w);
-			rooms[i].topSpace=(int) (r.nextFloat()*(6)+1);
-			rooms[i].rightSpace=(int) (r.nextFloat()*(6)+1);
-			rooms[i].downSpace=(int) (r.nextFloat()*(6)+1);
-			rooms[i].leftSpace=(int) (r.nextFloat()*(6)+1);
 		}
 		
-		System.out.println();
+		for(int i=0; i<7; i++){
+			corridorList.add(new Corridor(r,100,width,height));
+			Corridor a= corridorList.get(corridorList.size()-1);
+			for(int j=0; j<a.cells.size(); j++){
+				tempTerrain[a.cells.get(j).y][a.cells.get(j).x]=0;
+			}
+		}
+		
+		printAll();
+		
 		int tries, nstanze=0;
 		//sistemare stanze
 		for(int i=0; i<roomsnumber; i++){
 			tries=0;
 			rooms[i].x=2*((int) (0.5*(r.nextFloat()*(width-rooms[i].width-4)+2)));
 			rooms[i].y=2*((int) (0.5*(r.nextFloat()*(height-rooms[i].height-4)+2)));
-			while((inters(rooms[i], tempTerrain))&&(tries<100)){
+			while((inters(rooms[i]))&&(tries<100)){
+				System.out.println("RETRY");
 				rooms[i].x=2*((int) (0.5*(r.nextFloat()*(width-rooms[i].width-4)+2)));
 				rooms[i].y=2*((int) (0.5*(r.nextFloat()*(height-rooms[i].height-4)+2)));
 				tries++;
 			}
 			if(tries<100){
+				System.out.println("OK");
 				putRoom(rooms[i]);
 				roomList.add(rooms[i]);
 			}
 		}
 		
-		/*
-		//bind dei corridoi
-		System.out.println("Numero stanze: "+roomList.size());		
-		int n =(int) (Math.floor(Math.log(roomList.size())/Math.log(2)));		 		
-		for(int j=1; j<=n; j++)
-			for(int i=0; i<Math.floor(roomList.size()/Math.pow(2, j)); i++){
-				roomList.get((int) (i*Math.pow(2, j)+Math.floor(Math.pow(2, j-1)/2))).bind[j]=(int) (i*Math.pow(2, j)+Math.pow(2, j-1)+Math.floor(Math.pow(2, j-1)/2));
-				roomList.get((int) (i*Math.pow(2, j)+Math.pow(2, j-1)+Math.floor(Math.pow(2, j-1)/2))).bind[j]=(int) (i*Math.pow(2, j-1)+Math.floor(Math.pow(2, j-1)/2));				
-				System.out.println((int) (i*Math.pow(2, j)+Math.floor(Math.pow(2, j-1)/2))+" bind with "+(int) (i*Math.pow(2, j)+Math.pow(2, j-1)+Math.floor(Math.pow(2, j-1)/2)));
-			}
-		//ultima stanza, se dispari
-		if(roomList.size()%2!=0){
-			roomList.get(0).bind[1]=roomList.size()-1;
-			roomList.get(roomList.size()-1).bind[0]=0;
-		}
-		*/
+		printAll();
 		
-		for(int i=0; i<5; i++){
-			corridorList.add(new Corridor(r,100));
-			Corridor a= corridorList.get(corridorList.size()-1);
-			for(int j=0; j<a.cells.size(); j++){	
-				tempTerrain[a.cells.get(j).y][a.cells.get(j).x]=0;
-			}
-		}
-		
+	}
+	
+	public void printAll(){
 		for(int i=0; i<height; i++){
 			for(int j=0; j<width; j++){
-				terrain[i*2][j*2]=tempTerrain[i][j];
-				terrain[i*2+1][j*2]=tempTerrain[i][j];
-				terrain[i*2][j*2+1]=tempTerrain[i][j];
-				terrain[i*2+1][j*2+1]=tempTerrain[i][j];
+				terrain[i*corrWidth][j*corrWidth]=tempTerrain[i][j];
+				terrain[i*corrWidth+1][j*corrWidth]=tempTerrain[i][j];
+				terrain[i*corrWidth][j*corrWidth+1]=tempTerrain[i][j];
+				terrain[i*corrWidth+1][j*corrWidth+1]=tempTerrain[i][j];
 			}
 		}
-		for(int i=0; i<height*2; i++){
-			for(int j=0; j<width*2; j++){
+		for(int i=0; i<height*corrWidth; i++){
+			for(int j=0; j<width*corrWidth; j++){
 				if(terrain[i][j]==0) System.out.print(" ");
 				else System.out.print("#");
 			}
 			System.out.println();
 		}
-		
 	}
 	
 	public static void main(String[] args) throws Exception {
