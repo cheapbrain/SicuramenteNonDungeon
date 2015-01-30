76f1b2c3531fc03710c2973f35e1d36dc149f7f1
Kroj-0 <_ganryu_@libero.it>
2015-01-30 12:33:20 +0100

    Revert "Revert "Revert "Gen Dung 2.4"""
    This reverts commit 2aa23be77a5b6f62224c66d5eb1e3b4b4f26d10e.

--- src/happypotatoes/slickgame/worldgenerator/Generator.java
+++ src/happypotatoes/slickgame/worldgenerator/Generator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Generator {
 	public static Random r = new Random();
-	int seed=-5;
+	int seed=314159265;
 	public int width=75;
 	public int height=75;
 	public int corrWidth=2;
@@ -21,22 +21,23 @@ public class Generator {
 	public boolean inters(Room a){
 		int c=0;
 		for(int i=a.x; i<a.x+a.width-1; i++){ 
-			if((tempTerrain[i][a.y-1]==0)||(tempTerrain[i][a.y+a.height]==0))
+			if(tempTerrain[i][a.y-1]==0){
 				c++;
+			}
+			if(tempTerrain[i][a.y+a.height]==0){
+				c++;
+			}
 		}
 		for(int j=a.y; j<a.y+a.height-1; j++){ 
-			if((tempTerrain[a.x-1][j]==0)||(tempTerrain[a.x+a.width][j]==0))
+			if(tempTerrain[a.x-1][j]==0){
 				c++;
+			}
+			if(tempTerrain[a.x+a.width][j]==0){
+				c++;
+			}
 		}
-		
-		//intersezione tra stanze
-		for(int i=0; i<roomList.size(); i++){
-			Room b = roomList.get(i);
-			if((a.x+a.width>=b.x-1)&&(a.x-1<=b.x+b.width)&&(a.y+a.height>=b.y-1)&&(a.y-1<=b.y+b.height))
-				c=-1;
-		}
-			
-		if((c>0)&&(c<6)) return false;
+		System.out.print(c+" ");
+		if((c>0)&&(c<7)) return false;
 		else return true;
 	}
 	
@@ -71,18 +72,22 @@ public class Generator {
 			}
 		}
 		
+		//printAll();
+		
+		int tries, nstanze=0;
 		//sistemare stanze
-		int tries;
 		for(int i=0; i<roomsnumber; i++){
 			tries=0;
 			rooms[i].x=2*((int) (0.5*(r.nextFloat()*(width-rooms[i].width-4)+2)));
 			rooms[i].y=2*((int) (0.5*(r.nextFloat()*(height-rooms[i].height-4)+2)));
 			while((inters(rooms[i]))&&(tries<100)){
+				//System.out.println("RETRY");
 				rooms[i].x=2*((int) (0.5*(r.nextFloat()*(width-rooms[i].width-4)+2)));
 				rooms[i].y=2*((int) (0.5*(r.nextFloat()*(height-rooms[i].height-4)+2)));
 				tries++;
 			}
 			if(tries<100){
+				//System.out.println("OK");
 				putRoom(rooms[i]);
 				roomList.add(rooms[i]);
 			}
@@ -94,7 +99,10 @@ public class Generator {
 				terrain[i*corrWidth][j*corrWidth+1]=tempTerrain[i][j];
 				terrain[i*corrWidth+1][j*corrWidth+1]=tempTerrain[i][j];
 			}
-		}		
+		}
+		
+		//printAll();
+		
 	}
 	
 	public void printAll(){
