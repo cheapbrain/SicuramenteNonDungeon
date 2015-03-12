0358180502bb251f2ee27e722d667d931c40b8e9
cheapbrain <ermarigo96@gmail.com>
2015-03-12 09:15:22 +0100

    entity modificato

--- src/entity2/Entity.java
+++ src/entity2/Entity.java
@@ -7,26 +7,35 @@ import java.util.ArrayList;
 public final class Entity {
 	public long id;
 	public float x,y;	
-	public boolean enabled;
 	ArrayList<Component> components;
 	
 	public Component getComponent(Class<?> c){
-		for(int i=0; i<components.size(); i++) 
-			if(components.get(i).getClass()==c) return components.get(i);
+		for(Component component:components)
+			if (component.getClass()==c)
+				return component;
 		return null;
 	}
 	
 	public void addComponent(Component c){
+		if(!components.isEmpty()) {
+			int max = components.size();
+			for(int i=0;i<max;i++)
+				if (c.priority>components.get(i).priority) {
+					components.add(i, c);
+					return;
+				}
+		}
 		components.add(c);
 	}
 	
-	public void removeComponent(Class<?> c){
-	for(int i=0; i<components.size(); i++) 
-		if(components.get(i).getClass()==c) components.remove(i);
+	public void removeComponent(Component c){
+		components.remove(c);
 	}
 	
 	public void update(World w, long delta){
-		
+		for (Component component : components)
+			if (component.enabled)
+				component.update(w, delta);
 	}
 
 	public Entity(long id){
