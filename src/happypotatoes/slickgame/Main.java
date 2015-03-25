408619d10538e9c3a0af1a4ffe39036fdb9da880
cheapbrain <ermarigo96@gmail.com>
2015-03-25 15:00:33 +0100

    lol
    This reverts commit 228e4e3145cfbc32cb18f00d63f5914127b5f4ab.
    
    Revert "light v1"
    
    This reverts commit
    38a447fd6b7db25218e5214c7579e9db9bd3ccc4.

--- src/happypotatoes/slickgame/Main.java
+++ src/happypotatoes/slickgame/Main.java
@@ -1,37 +1,16 @@
 package happypotatoes.slickgame;
 
-import java.awt.*; import java.awt.event.*;
+import org.newdawn.slick.AppGameContainer;
 
-public class Main extends java.applet.Applet implements ActionListener { Picture pic;
+public class Main {
 
-public void init () { setLayout (new BorderLayout ());
+	public static void main(String[] args) throws Exception {
+		
+		AppGameContainer app = new AppGameContainer(new GameStateController());
+		app.setDisplayMode(800, 600, false);
+        app.start();
 
-Image im = createImage (200, 150); pic = new Picture (im);
 
-Panel p = new Panel (); p.add (pic); add (p, BorderLayout.NORTH);
 
-Button b = new Button ("Draw"); b.addActionListener (this);
-
-p = new Panel (); p.add (b); add (p, BorderLayout.SOUTH); }
-
-public void actionPerformed (ActionEvent e) { pic.draw (); } }
-
-class Picture extends Canvas { private Image buffer; private Graphics context; private int height, width;
-
-Picture (Image buffer) { this.buffer = buffer; context = buffer.getGraphics ();
-
-width = buffer.getWidth (this); height = buffer.getHeight (this); }
-
-public void draw () { for (int row = 0; row < height; row++) for (int col = 0; col < width; col++) { context.setColor (new Color (rnd (256), rnd (256), rnd (256)));
-
-context.drawLine (col, row, col, row); }
-
-repaint (); }
-
-public Dimension getPreferredSize () { return new Dimension (width, height); }
-
-public void paint (Graphics g) { g.drawImage (buffer, 0, 0, this); }
-
-public void update (Graphics g) { paint (g); }
-
-private int rnd (int limit) { return (int) (Math.random () * limit); } }
+	}
+}
